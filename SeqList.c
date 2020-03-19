e2a16f9ee7c7b44f70901f77f9fe1ab0e930dc52
syf814986678 <814986678@qq.com>
2020-03-19 14:34:23 +0800

    完成

--- SeqList.c
+++ SeqList.c
@@ -24,29 +24,28 @@ int DelList(SeqList *L,int i){
 int DelSame(SeqList *l){
     int i,j;
     if(l->last<=0) return ok;
-    for(i=0,j=0;i<=l->last;i++){
+    for(i=0,j=1;i<l->last;i++){
         if(l->elem[i]!=l->elem[i+1]){
-           // l->elem[j+1]=l->elem[i+1]; 
-            l->elem[j]=l->elem[i];
-          //  printf("循环内%d\n",l->elem[i+1]);
-           // printf("循环内%d\n",l->elem[i+2]);
-            j++;
+            l->elem[j++]=l->elem[i+1];
+            //l->elem[j+1]=l->elem[i+1]; 
         }
        // l->last=j-1;
     }
     l->last=j-1;
+    return ok;
 }
-
 int main(){
 
     SeqList SeqList;
     SeqList.elem[0]=1;
-    SeqList.elem[1]=1;
+    SeqList.elem[1]=2;
     SeqList.elem[2]=2;
-    SeqList.elem[3]=4;
+    SeqList.elem[3]=2;
     SeqList.elem[4]=4;
-    SeqList.elem[5]=5;
-    SeqList.last=5;
+    SeqList.elem[5]=6;
+    SeqList.elem[6]=6;
+    SeqList.elem[7]=6;
+    SeqList.last=7;
     //int e;
    // DelList(&SeqList,3);
     DelSame(&SeqList);
